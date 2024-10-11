d1d3af3e7b28c0f24c5ebccc074899038ab99866
jesuscb123 <cbjesus@iesrafaelalberti.es>
2024-10-11 12:50:44 +0200

    Mejora de código ejercicio 30 y comentado.

--- src/ejercicios_básicos_añadidos/ej30.py
+++ src/ejercicios_básicos_añadidos/ej30.py
@@ -1,14 +1,17 @@
 #Escribir un programa que determine si un número es primo
 
+#He realizado una función que indica si es par o impar. Para ello dividimos el número dado entre 2 y si el resto es igual a 0 será primo, eso hará que nos lleve a otra función creada llamada es_par_y_primo(num1). Si el resto no es igual a 0 debemos comprobar con otra función llamada es_impar_y_primo.
+
 def parimpar(num1):
     par = num1 % 2
     if par == 0:
        es_par_y_primo(num1)
     else:
-        es_divisible(num1)
+        es_impar_y_primo(num1)
 
 
 
+#Si es par, nos llevará a la siguiente función que indica si el número dado es menor o igual a 1 o es mayor que 2, el número no es primo. Si el número es igual a 2 o mayor que 1, el número es primo.
 
 def es_par_y_primo(num1):
     if num1 <= 1 or num1 > 2:
@@ -19,6 +22,7 @@ def es_par_y_primo(num1):
 
 
 
+#Si es impar, nos llevará a la siguiente función que indica si el número dado es menor o igual que 1 el número no será primo. Sino, si el número es igual a 2 o el número es mayor que 1, tenemos que comprobar si es divisible por más de un número que no sea 1 o él mismo. Para ello he creado la función es_divisible.
 
 def es_impar_y_primo(num1):
     if num1 <= 1:
@@ -29,6 +33,7 @@ def es_impar_y_primo(num1):
 
 
 
+#He creado un rango del 1 al 30 saltándose dos números para siempre dividirlos por números impares. Va a ir dividiendo uno por uno, cuando divide, convertirá ese int en un str para poder usar la función .find. Le he indicado que en el momento que encuentre un "."(eso indica que no será divisible debido a que no da número entero sino decimales) salga del bucle e indique que el número es primo.
 
 def es_divisible(num1):
     for i in range (1,30,2):
@@ -37,10 +42,9 @@ def es_divisible(num1):
         encuentra_punto = es_divisible_str.find(".")
     if es_divisible_str.find("."):
         print("El número es primo")
-    else: 
-        print("El número no 3es primo")
     
 
+#El main solo llamará a la función parimpar debido a que las demás funciones trabajaran llamándose entre ellas y devolviendo el resultado con el print. 
 
 def main():
     num1 = int(input("introduce un número: " ))
