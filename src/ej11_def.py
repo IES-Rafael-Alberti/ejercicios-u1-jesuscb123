e97ff07e868b7a7663936fdc727605f33c6e555a
jesuscb123 <cbjesus@iesrafaelalberti.es>
2024-10-12 16:18:03 +0200

    He comentado algunos ejercicios, terminar de comentar los demás.

--- src/ej11_def.py
+++ src/ej11_def.py
@@ -1,17 +1,25 @@
 #Recibe un número y retorna una cadena de caracteres con el resultado de la función.
 
+#He creado una función que hace que la variable suma almacene el número introducido por la suma de el número +1 y luego lo divida todo entre 2. Luego ese valor se retorna debido a que main hará un print de la función y se mostrará.
 def serie (num):
     suma = (num * (num +1)) / 2
     return suma
+
+
+#Una función para comprobar si el número introducido es positivo, si el número es negativo seguirá pidiendo un número positivo.
 def es_positivo(num):
     while num < 0: 
         num = int(input("Error, introduce un número positivo: "))
     return num
+
+
     
 def main ():
     num = int(input("Introduce un número positivo: "))
     es_positivo(num)
     print (serie(num))
+
+
    
 if __name__ == "__main__":
     main()
--- src/ej12.py
+++ src/ej12.py
@@ -1,10 +1,16 @@
 #Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, y muestre por pantalla la frase Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales.
-peso = float (input("Introduce tu peso: ")) #Introducimos un peso, ponemos float debido a que pueden tener decimales.
-while peso < 0:                             # mientras peso sea negativo, señalar un error debido a que no existe peso en negativo.
+
+#Introducimos un peso, ponemos float debido a que pueden tener decimales.
+peso = float (input("Introduce tu peso: ")) 
+# mientras peso sea negativo, señalar un error debido a que no existe peso en negativo.
+while peso < 0:                             
     peso = float (input("Error, introduce un peso correcto: "))
 altura = float (input("Introduce tu altura: ")) 
-while altura < 0:                           #Mientras la altura sea negativa, señalar un error debido a que no existe altura en negativa.
+#Mientras la altura sea negativa, señalar un error debido a que no existe altura en negativa.
+while altura < 0:                           
     altura = float (input("Error, introduce una altura correcta: "))
-indicemasa = peso / altura ** 2             #El indice de masa corporal se mide diviendo el peso (en kg) por la altura (m2)
-redondeado = round (indicemasa, 2)          #para redondear, he creado otra variable llamada "redondeado" y round hace que redondees a los decimales que quieras.
+#El indice de masa corporal se mide diviendo el peso (en kg) por la altura (m2)
+indicemasa = peso / altura ** 2        
+#para redondear, he creado otra variable llamada "redondeado" y round hace que redondees a los decimales que quieras.    
+redondeado = round (indicemasa, 2)          
 print (f"Tu indice de masa corporal es: {redondeado}") 
