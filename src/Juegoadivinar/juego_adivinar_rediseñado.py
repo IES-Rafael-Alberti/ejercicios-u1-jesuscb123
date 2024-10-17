bcea9e7d4079e4828d835a00ab52f2f89ec09fe3
jesuscb123 <cbjesus@iesrafaelalberti.es>
2024-10-18 00:06:34 +0200

    Todos ejercicios terminados excepto el 32. Juego adivinar terminado y funcionando.

--- src/Juegoadivinar/juego_adivinar_rediseñado.py
+++ src/Juegoadivinar/juego_adivinar_rediseñado.py
@@ -1,13 +1,66 @@
 import random
 
-def introduce_numero(msj:str):
-    numero_user = input
 
+def mensaje_error(num_user):
+    num_user = input("ERROR, INTRODUCE UN NÚMERO VÁLIDO: ")
+    return num_user
+
+
+def adivina(num_user,num_pc):
+        diferencia = int(num_pc - num_user)
+        diferencia_absoluta = abs(diferencia)
+        if num_user == num_pc:
+            return True
+        elif diferencia < 5:
+            print("ARDIENDO")
+        elif diferencia < 10:
+            print("CALENTITO...")
+        elif diferencia < 20:
+            print("COLACAO TEMPLAO")
+        elif diferencia < 50:
+            print("Estas cerca y a la vez lejos...")
+        elif diferencia < 70 or diferencia > 70: 
+            print ("CONGELAO")
+        
+
+        
+def es_mayor_de_100(num_user):
+    num_user = int(num_user)
+    if num_user > 100:
+        return True
+
+
+
+def comprobar_entero(num_user: str):
+    if num_user.startswith("-"):
+        num_user= num_user[1:]
+    elif num_user.count("."):
+        return False
+    else:
+        num_user.strip
+        return num_user.isdigit()
+
+
+
+def introduce_numero():
+    num_user = input("Introduce un número: ")
+    while not comprobar_entero(num_user) or es_mayor_de_100(num_user):
+        num_user = mensaje_error(num_user)
+    num_user = int(num_user)
+    return num_user
+    
+    
 
 def main():
-    num_pc = random.randint(1,100)
+    num_pc = random.randint(0,100)
+    num_user = introduce_numero()
+    while not adivina(num_user,num_pc):
+        num_user = introduce_numero()
+    print("LO CONSEGUISTE")
 
-
+        
+        
+    
 
 
 
--- src/ej05_def2.py
+++ src/ej05_def2.py
@@ -1,12 +1,13 @@
 #La función calcula_precio(importe: float, iva: float) -> str recibe el importe y el iva, si el iva está fuera del rango 0-100 se aplicará el 21%, y retornará una cadena de caracteres con el iva y el precio con iva mostrando solo 2 posiciones decimales. Ejemplo: calcula_precio(100, 21) -> "El precio final del artículo con IVA (21.00) es 121.00€."
 
 def calcular_iva_21(importe,iva):
-    importe_con_iva = importe * (21 / 100)
-    return f"El precio final con iva 21.00 es: {round(importe_con_iva,2)}"
+    iva = 21
+    importe_con_iva = importe * (1 + iva / 100)
+    return f"El precio final con iva {iva:.2f} es: {importe_con_iva:.2f}"
 
 def calcular_iva(importe,iva):
-    importe_con_iva = importe * (iva / 100)
-    return f"El precio final con iva {iva} es: {round(importe_con_iva,2)}"
+    importe_con_iva = importe * (1+ iva / 100)
+    return f"El precio final con iva {iva:.2f} es: {importe_con_iva:.2f}"
 
 def comprobar_si_21(iva):
     if iva < 0 or iva > 100:
